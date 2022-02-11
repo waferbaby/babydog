a21028019ca355a64e434cff053bff16fed22fa7
Daniel Bogan <d@waferbaby.com>
2022-02-11 14:53:21 +0000

    Add more options, remove the strike details idea

--- src/Strike.svelte
+++ src/Strike.svelte
@@ -1,29 +1,33 @@
 <script>
-import nouns from '../corpora/data/words/nouns.json'
-import verbs from '../corpora/data/words/verbs.json'
+  import tracery from 'tracery-grammar'
+  import grammar from './grammar.json'
 
-import tracery from 'tracery-grammar'
-import grammar from './grammar.json'
+  import nouns from '../corpora/data/words/nouns.json'
+  import verbs from '../corpora/data/words/verbs.json'
+  import clothing from '../corpora/data/objects/clothing.json'
+  import instruments from '../corpora/data/music/instruments.json'
+  import animals from '../corpora/data/animals/common.json'
 
-grammar['noun'] = nouns.nouns
-grammar['present_verb'] = verbs.verbs.map((v) => v.present)
-grammar['past_verb'] = verbs.verbs.map((v) => v.past)
+  grammar['noun'] = nouns.nouns
+  grammar['past_verb'] = verbs.verbs.map((v) => v.past)
+  grammar['present_verb'] = verbs.verbs.map((v) => v.present)
+  grammar['clothing'] = clothing.clothes
+  grammar['instrument'] = instruments.instruments
+  grammar['animal'] = animals.animals
 
-let generator = tracery.createGrammar(grammar)
-let strike_name = ''
-let strike_details = ''
+  let generator = tracery.createGrammar(grammar)
+  let strike_name = ''
 
-generator.addModifiers(tracery.baseEngModifiers);
+  generator.addModifiers(tracery.baseEngModifiers);
 
-function generateStrike() {
-  strike_name = generator.flatten('#strike_name#')
-}
+  function generateStrike() {
+    strike_name = generator.flatten('#strike_name#')
+  }
 
-generateStrike();
+  generateStrike();
 </script>
 
 <div on:click={generateStrike}>
   <h1>Operation...</h1>
   <strong>{strike_name}?</strong>
-  <p>{strike_details}</p>
 </div>
