7861354d4bb4549773a6d72a7c2f5bd099d468ff
Daniel Bogan <d@waferbaby.com>
2025-08-14 07:18:14 +0000

    Add the check_authentication method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def check_authentication
+    unless session[:bungie_account].present?
+      session[:bungie_state] = SecureRandom.hex(10)
+      redirect_to(Restiny.get_authorise_url(redirect_url: url_for(:login), state: session[:bungie_state]), allow_other_host: true)
+    end
+  end
 end
