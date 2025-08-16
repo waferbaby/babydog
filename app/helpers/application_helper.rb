6bf67593b7098006d3e7ff61fdbcd747e11f1cf8
Daniel Bogan <d@waferbaby.com>
2025-08-16 09:56:24 +0000

    Add the auth/membership helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_membership
+    @current_membership ||= BungieMembership.find_by(membership_id: session[:bungie_membership_id])
+  end
+
+  def logged_in?
+    session[:bungie_access_token].present? && current_membership.present?
+  end
 end
