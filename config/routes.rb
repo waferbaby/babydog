ddff70a1246aa3ddd0fde36d8c7640ae806faa83
Daniel Bogan <d@waferbaby.com>
2025-08-16 08:19:41 +0000

    Update the auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "home#index"
 
-  get '/login/', to: 'home#login', as: :login
+  get '/login/', to: 'application#login', as: :login
+  get '/logout/', to: 'application#logout', as: :logout
+
   get '/vault/', to: 'vault#index'
 end
