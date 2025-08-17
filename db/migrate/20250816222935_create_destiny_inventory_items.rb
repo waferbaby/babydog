a031304357acad9356afd2c973749cc30321f58c
Daniel Bogan <d@waferbaby.com>
2025-08-17 03:11:10 +0000

    Add the initial fields

--- db/migrate/20250816222935_create_destiny_inventory_items.rb
+++ db/migrate/20250816222935_create_destiny_inventory_items.rb
@@ -1,6 +1,14 @@
 class CreateDestinyInventoryItems < ActiveRecord::Migration[8.0]
   def change
     create_table :destiny_inventory_items do |t|
+      t.bigint :bungie_hash
+      t.string :name
+      t.string :description
+      t.string :flavour_text
+      t.integer :tier_type
+      t.boolean :is_featured
+      t.boolean :is_holofoil
+      t.boolean :is_adept
       t.timestamps
     end
   end
