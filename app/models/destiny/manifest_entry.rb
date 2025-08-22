fc5fe74cc55f68c650fbf43c37ffc0ca21671677
Daniel Bogan <d@waferbaby.com>
2025-08-22 08:14:26 +0000

    Symbolise keys, default to an empty hash

--- app/models/destiny/manifest_entry.rb
+++ app/models/destiny/manifest_entry.rb
@@ -3,15 +3,16 @@ module Destiny
     self.abstract_class = true
 
     def self.import(payload)
-      puts self.class
+      payload.deep_symbolize_keys!
+
       item = self.find_or_create_by!(bungie_hash: payload[:hash])
-      item.update!(self.payload_to_update(payload))
+      item.update!(self.payload_to_update_params(payload))
     end
 
     private
 
-    def self.payload_to_update(payload)
-      payload.delete(:hash)
+    def self.payload_to_update_params(payload)
+      {}
     end
   end
 end
