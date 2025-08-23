3fccab92ede17533dc2900f9c7897bf93dda0832
Daniel Bogan <d@waferbaby.com>
2025-08-23 06:59:06 +0000

    Ad the payload_to_attributes method

--- app/models/destiny/stat.rb
+++ app/models/destiny/stat.rb
@@ -1,4 +1,10 @@
 module Destiny
   class Stat < ManifestEntry
+    def self.payload_to_attributes(payload)
+      super(payload).merge({
+        aggregation_type: :aggregationType,
+        category: :statCategory
+      })
+    end
   end
 end
