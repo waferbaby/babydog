c9c02a4f39cd5b4c080058e5a4acc38ae24a1b72
Daniel Bogan <d@waferbaby.com>
2025-08-23 07:00:07 +0000

    Update payload_to_attributes to use the super method

--- app/models/destiny/item_category.rb
+++ app/models/destiny/item_category.rb
@@ -2,16 +2,11 @@ module Destiny
   class ItemCategory < ManifestEntry
     def self.payload_to_attributes(payload)
       super(payload).merge({
-        name: [:displayProperties, :name],
-        description: [:displayProperties, :description],
         item_type: :grantDestinyItemType,
         item_sub_type: :grantDestinySubType,
         guardian_type: :grantDestinyClass,
         breaker_type: :grantDestinyBreakerType,
-        is_plug: :isPlug,
-        is_deprecated: :deprecated,
-        is_redacted: :redacted,
-        is_blacklisted: :blacklisted
+        is_plug: :isPlug
       })
     end
   end
