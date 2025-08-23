54267056d13153c10bf29836994c257e9cd5c482
Daniel Bogan <d@waferbaby.com>
2025-08-23 05:49:11 +0000

    Update to payload_to_attributes

--- app/models/destiny/inventory_item.rb
+++ app/models/destiny/inventory_item.rb
@@ -1,22 +1,18 @@
 module Destiny
   class InventoryItem < ManifestEntry
-    def self.payload_to_update_params(payload)
-      {
-        name: payload.dig(:displayProperties, :name),
-        description: payload.dig(:displayProperties, :description),
-        flavour_text: payload[:flavorText],
-        item_type: payload[:itemType],
-        item_subtype: payload[:itemSubtype],
-        guardian_type: payload[:classType],
-        tier_type: payload[:tierType],
-        breaker_type: payload[:breakerType],
-        is_equippable: payload[:isEquippable],
-        is_featured: payload[:isFeatured],
-        is_holofoil: payload[:isHolofoil],
-        is_adept: payload[:isAdept],
-        is_redacted: payload[:redacted],
-        is_blacklisted: payload[:blacklisted]
-      }
+    def self.payload_to_attributes(payload)
+      super(payload).merge({
+        flavour_text: :flavorText,
+        item_type: :itemType,
+        item_subtype: :itemSubtype,
+        guardian_type: :classType,
+        tier_type: :tierType,
+        breaker_type: :breakerType,
+        is_equippable: :isEquippable,
+        is_featured: :isFeaturedItem,
+        is_holofoil: :isHolofoil,
+        is_adept: :isAdept
+      })
     end
   end
 end
