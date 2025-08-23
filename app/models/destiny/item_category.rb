5cdd8d0c4845317c339c0eb8725cbe767cee2c61
Daniel Bogan <d@waferbaby.com>
2025-08-23 05:49:27 +0000

    Update to payload_to_attributes

--- app/models/destiny/item_category.rb
+++ app/models/destiny/item_category.rb
@@ -1,7 +1,18 @@
 module Destiny
   class ItemCategory < ManifestEntry
-    def self.payload_to_update_params(payload)
-      {}
+    def self.payload_to_attributes(payload)
+      super(payload).merge({
+        name: [:displayProperties, :name],
+        description: [:displayProperties, :description],
+        item_type: :grantDestinyItemType,
+        item_sub_type: :grantDestinySubType,
+        guardian_type: :grantDestinyClass,
+        breaker_type: :grantDestinyBreakerType,
+        is_plug: :isPlug,
+        is_deprecated: :deprecated,
+        is_redacted: :redacted,
+        is_blacklisted: :blacklisted
+      })
     end
   end
 end
