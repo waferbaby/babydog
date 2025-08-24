0fc66473ee6adbe7b067b70a579e3e55d5c15f28
Daniel Bogan <d@waferbaby.com>
2025-08-24 05:29:13 +0000

    Move the logged_in? and current_membership methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,17 @@
 class ApplicationController < ActionController::Base
   allow_browser versions: :modern
+  helper_method :logged_in?, :current_membership
 
   def check_authentication
-    unless session[:bungie_membership_id].present?
-      session[:redirect_to] = request.original_fullpath
-      session[:bungie_state] = SecureRandom.hex(10)
+    return if logged_in?
 
-      redirect_to(
-        Restiny.get_authorise_url(redirect_url: url_for(:login), state: session[:bungie_state]),
-        allow_other_host: true
-      )
-    end
+    session[:redirect_to] = request.original_fullpath
+    session[:bungie_state] = SecureRandom.hex(10)
+
+    redirect_to(
+      Restiny.get_authorise_url(redirect_url: url_for(:login), state: session[:bungie_state]),
+      allow_other_host: true
+    )
   end
 
   def login
@@ -22,14 +23,14 @@ class ApplicationController < ActionController::Base
     token_result = Restiny.request_access_token(params[:code])
     member_result = Restiny.get_primary_user_membership(token_result['membership_id'])
 
-    membership = Destiny::Membership.find_or_initialize_by(membership_id: token_result['membership_id'])
+    membership = Destiny::Membership.find_or_initialize_by(membership_id: member_result['membershipId'])
     membership.update!(
       membership_type: member_result['membershipType'],
       username: member_result['bungieGlobalDisplayName']
     )
 
     session[:bungie_access_token] = token_result['access_token']
-    session[:bungie_membership_id] = token_result['membership_id']
+    session[:bungie_membership_id] = membership.id
 
     redirect_to session[:redirect_to] || :root
   rescue Restiny::AuthenticationError => e
@@ -42,5 +43,13 @@ class ApplicationController < ActionController::Base
   def logout
     reset_session
     redirect_to :root
+  end
+
+  def current_membership
+    @current_membership ||= Destiny::Membership.find_by(id: session[:bungie_membership_id])
+  end
+
+  def logged_in?
+    session[:bungie_access_token].present? && current_membership.present?
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def current_membership
-    @current_membership ||= Destiny::Membership.find_by(membership_id: session[:bungie_membership_id])
-  end
-
-  def logged_in?
-    session[:bungie_access_token].present? && current_membership.present?
-  end
 end
