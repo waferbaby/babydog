945397180e2a2f78647a2e75fb96892663217aaa
Daniel Bogan <d@waferbaby.com>
2025-08-26 08:38:08 +0000

    Switch to the migration

--- db/migrate/20250816222930_create_destiny_traits.rb
+++ db/migrate/20250816222930_create_destiny_traits.rb
@@ -3,13 +3,7 @@ require_relative './destiny/migration'
 class CreateDestinyTraits < ActiveRecord::Migration[8.0]
   def change
     create_table :destiny_traits do |t|
-      t.bigint :bungie_hash
-      t.bigint :index
-      t.string :name
-      t.string :description
-      t.boolean :is_redacted
-      t.boolean :is_blacklisted
-      t.timestamps
+      Destiny::Migration.add_common_fields(t)
     end
   end
 end
