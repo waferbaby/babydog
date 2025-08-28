8152852b02c6e4bf78f144d7368ee2e0338c3465
Daniel Bogan <d@waferbaby.com>
2025-08-28 09:47:06 +0000

    Add the item traits association, fix equippable

--- app/models/destiny/inventory_item.rb
+++ app/models/destiny/inventory_item.rb
@@ -1,12 +1,15 @@
 module Destiny
   class InventoryItem < ManifestEntry
+    has_many :inventory_item_traits, foreign_key: :trait_hash
+    has_many :traits, through: :inventory_item_traits
+
     def self.payload_to_attributes(payload)
       super(payload).merge({
         flavour_text: :flavorText,
         guardian_type: :classType,
         tier_type: :tierType,
         breaker_type: :breakerType,
-        is_equippable: :isEquippable,
+        is_equippable: :equippable,
         is_featured: :isFeaturedItem,
         is_holofoil: :isHolofoil,
         is_adept: :isAdept
