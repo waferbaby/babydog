51d44d4b266044e6bc659596d7f0d55e34a4b170
Daniel Bogan <d@waferbaby.com>
2025-08-28 09:47:34 +0000

    Add the item trait association

--- app/models/destiny/trait.rb
+++ app/models/destiny/trait.rb
@@ -1,4 +1,6 @@
 module Destiny
   class Trait < ManifestEntry
+    has_many :inventory_item_traits, foreign_key: :inventory_item_hash
+    has_many :inventory_items, through: :inventory_item_traits
   end
 end
