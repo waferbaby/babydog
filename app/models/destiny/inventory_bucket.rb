4a0fee02852a3cf36a1473e96a326b1ca466ec4d
Daniel Bogan <d@waferbaby.com>
2025-09-01 08:57:56 +0000

    Link up inventory items and buckets

--- app/models/destiny/inventory_bucket.rb
+++ app/models/destiny/inventory_bucket.rb
@@ -1,4 +1,5 @@
 module Destiny
   class InventoryBucket < ManifestEntry
+    has_many :inventory_items, foreign_key: :inventory_bucket_hash, primary_key: :bungie_hash
   end
 end
--- app/models/destiny/inventory_item.rb
+++ app/models/destiny/inventory_item.rb
@@ -2,6 +2,7 @@ module Destiny
   class InventoryItem < ManifestEntry
     has_many :inventory_item_traits, foreign_key: :inventory_item_hash, primary_key: :bungie_hash
     has_many :traits, through: :inventory_item_traits
+    belongs_to :inventory_bucket, foreign_key: :inventory_bucket_hash, primary_key: :bungie_hash
 
     def self.payload_to_attributes(payload)
       super(payload).merge({
@@ -12,17 +13,20 @@ module Destiny
         is_equippable: :equippable,
         is_featured: :isFeaturedItem,
         is_holofoil: :isHolofoil,
-        is_adept: :isAdept
+        is_adept: :isAdept,
+        inventory_bucket_hash: [:inventory, :bucketTypeHash]
       })
     end
 
     def self.link_associations(payload)
       if payload.key?(:traitHashes)
+        Destiny::InventoryItemTrait.where(inventory_item_hash: payload[:hash]).delete_all
+
         payload[:traitHashes].each do |trait_hash|
-          Destiny::InventoryItemTrait.upsert({
+          Destiny::InventoryItemTrait.create!({
             inventory_item_hash: payload[:hash],
             trait_hash: trait_hash
-          }, unique_by: [:inventory_item_hash, :trait_hash])
+          })
         end
       end
     end
