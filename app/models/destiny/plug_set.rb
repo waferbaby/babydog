f46065b5384ec48101aaba39bd104a74de0d00b3
Daniel Bogan <d@waferbaby.com>
2025-09-03 07:18:44 +0000

    Add in payload_to_attributes and link_associations

--- app/models/destiny/plug_set.rb
+++ app/models/destiny/plug_set.rb
@@ -1,4 +1,25 @@
 module Destiny
   class PlugSet < ManifestEntry
+    def self.payload_to_attributes(payload)
+      super(payload).merge({
+        is_fake: :isFakePlugSet
+      })
+    end
+
+    def self.link_associations(payload)
+      if payload.key?(:reusablePlugItems)
+        Destiny::PlugSetItem.where(plug_set_hash: payload[:hash]).delete_all
+
+        payload[:reusablePlugItems].each do |plug_item|
+          Destiny::PlugSetItem.import_entry(
+            plug_set_hash: payload[:hash],
+            inventory_item_hash: plug_item[:plugItemHash],
+            can_roll: plug_item[:currentlyCanRoll],
+            weight: plug_item[:weight],
+            alternate_weight: plug_item[:alternateWeight]
+          )
+        end
+      end
+    end
   end
 end
