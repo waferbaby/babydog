85c77e4381ce821b10445c295e0b4ecf420f7010
Daniel Bogan <d@waferbaby.com>
2025-09-03 09:05:29 +0000

    Fix bucket hash

--- db/migrate/20250824052508_create_destiny_inventory_vault_items.rb
+++ db/migrate/20250824052508_create_destiny_inventory_vault_items.rb
@@ -3,9 +3,9 @@ class CreateDestinyInventoryVaultItems < ActiveRecord::Migration[8.0]
     create_table :destiny_inventory_vault_items do |t|
       t.bigint :bungie_hash
       t.bigint :instance_hash
+      t.bigint :inventory_bucket_hash
       t.bigint :location
       t.belongs_to :membership
-      t.belongs_to :inventory_bucket
       t.timestamps
 
       t.index :bungie_hash, unique: true
