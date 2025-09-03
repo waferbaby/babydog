8a750925195c4ace5cc90015c0662fdd4a2bb66e
Daniel Bogan <d@waferbaby.com>
2025-09-03 06:35:16 +0000

    Switch parent class, add the unique_keys override

--- app/models/destiny/inventory_item_category.rb
+++ app/models/destiny/inventory_item_category.rb
@@ -1,6 +1,12 @@
 module Destiny
-  class InventoryItemCategory < ApplicationRecord
+  class InventoryItemCategory < ManifestEntry
     belongs_to :inventory_item, primary_key: :bungie_hash, foreign_key: :inventory_item_hash
     belongs_to :category, primary_key: :bungie_hash, foreign_key: :category_hash
+
+    private
+
+    def self.unique_keys
+      [ :inventory_item_hash, :category_hash ]
+    end
   end
 end
--- app/models/destiny/inventory_item_trait.rb
+++ app/models/destiny/inventory_item_trait.rb
@@ -1,6 +1,12 @@
 module Destiny
-  class InventoryItemTrait < ApplicationRecord
+  class InventoryItemTrait < ManifestEntry
     belongs_to :inventory_item, primary_key: :bungie_hash, foreign_key: :inventory_item_hash
     belongs_to :trait, primary_key: :bungie_hash, foreign_key: :trait_hash
+
+    private
+
+    def self.unique_keys
+      [ :inventory_item_hash, :trait_hash ]
+    end
   end
 end
