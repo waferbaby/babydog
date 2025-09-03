96fbf113a796598c2170c90cd1e9e3c1cbd42a89
Daniel Bogan <d@waferbaby.com>
2025-09-03 07:19:03 +0000

    Add the plug set and item associations and unique_keys method

--- app/models/destiny/plug_set_item.rb
+++ app/models/destiny/plug_set_item.rb
@@ -1,4 +1,12 @@
 module Destiny
-  class PlugSetItem < ApplicationRecord
+  class PlugSetItem < ManifestEntry
+    belongs_to :plug_set, primary_key: :bungie_hash, foreign_key: :plug_set_hash
+    belongs_to :inventory_item, primary_key: :bungie_hash, foreign_key: :inventory_item_hash
+
+    private
+
+    def self.unique_keys
+      [ :plug_set_hash, :inventory_item_hash ]
+    end
   end
 end
