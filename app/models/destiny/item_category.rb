3199c69675f7b0a87ca8657be7cbce99d4b94a76
Daniel Bogan <d@waferbaby.com>
2025-09-03 06:36:35 +0000

    Add the item associations

--- app/models/destiny/item_category.rb
+++ app/models/destiny/item_category.rb
@@ -1,5 +1,8 @@
 module Destiny
   class ItemCategory < ManifestEntry
+    has_many :inventory_item_categories, foreign_key: :category_hash, primary_key: :bungie_hash
+    has_many :inventory_items, through: :inventory_item_categories
+
     def self.payload_to_attributes(payload)
       super(payload).merge({
         is_plug: :isPlug
