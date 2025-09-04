8c4cb0e90c111c682e873b29c943b61b3b6b41e9
Daniel Bogan <d@waferbaby.com>
2025-09-04 23:54:59 +0000

    Add the custom import methods

--- app/models/destiny/inventory_vault_item.rb
+++ app/models/destiny/inventory_vault_item.rb
@@ -1,4 +1,20 @@
 module Destiny
-  class InventoryVaultItem < ApplicationRecord
+  class InventoryVaultItem < ManifestEntry
+    def self.payload_to_attributes(payload)
+      {
+        inventory_item_hash: :itemHash,
+        instance_hash: :itemInstanceId,
+        inventory_bucket_hash: :bucketHash,
+        quantity: :quantity,
+        location: :location,
+        bind_status: :bindStatus,
+        is_lockable: :lockable,
+        override_style_hash: :overrideStyleItemHash
+      }
+    end
+
+    def self.unique_keys
+      [ :membership_id, :inventory_item_hash ]
+    end
   end
 end
