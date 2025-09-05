dc9b497900a5f5e6987d253a42c70713e47f2019
Daniel Bogan <d@waferbaby.com>
2025-09-05 22:40:02 +0000

    Add more manifests, pass the collection values directly

--- app/jobs/import_manifest_job.rb
+++ app/jobs/import_manifest_job.rb
@@ -5,6 +5,9 @@ class ImportManifestJob < ApplicationJob
     sources = Rails.cache.fetch("babydog/manifests/#{Date.today}") do
       Restiny.download_manifest_json(definitions: [
         Restiny::ManifestDefinition::STAT,
+        Restiny::ManifestDefinition::STAT_GROUP,
+        Restiny::ManifestDefinition::DAMAGE_TYPE,
+        Restiny::ManifestDefinition::ENERGY_TYPE,
         Restiny::ManifestDefinition::TRAIT,
         Restiny::ManifestDefinition::ITEM_CATEGORY,
         Restiny::ManifestDefinition::PLUG_SET,
@@ -17,7 +20,8 @@ class ImportManifestJob < ApplicationJob
       object_type = "Destiny::#{manifest.gsub(/Destiny|Definition/, '')}".constantize
 
       begin
-        object_type.import_manifest(JSON.parse(File.read(file_path)))
+        manifest = JSON.parse(File.read(file_path))
+        object_type.import_collection(manifest.values)
       rescue JSON::ParserError => e
         Rails.logger.error("Failed to parse #{file_path}")
       end
