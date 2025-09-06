8ce20974f9a6d60e8d9eb55f53b4b20caddbfea9
Daniel Bogan <d@waferbaby.com>
2025-09-06 07:37:51 +0000

    Add the membership ad item associations and initial delegate methods

--- app/models/destiny/inventory_vault_item.rb
+++ app/models/destiny/inventory_vault_item.rb
@@ -1,5 +1,10 @@
 module Destiny
   class InventoryVaultItem < ManifestEntry
+    belongs_to :membership, primary_key: :membership_id
+    has_one :inventory_item, foreign_key: :bungie_hash, primary_key: :inventory_item_hash
+
+    delegate :bungie_hash, :name, :description, to: :inventory_item
+
     def self.payload_to_attributes(payload)
       {
         inventory_item_hash: :itemHash,
