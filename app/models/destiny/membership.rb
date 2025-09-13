7a7468b629db0339898dd285d2bd928eb5e45a33
Daniel Bogan <d@waferbaby.com>
2025-09-13 09:26:26 +0000

    Change the item association, add the recent character method

--- app/models/destiny/membership.rb
+++ app/models/destiny/membership.rb
@@ -1,10 +1,14 @@
 module Destiny
   class Membership < ManifestEntry
-    has_many :vault_items, class_name: "InventoryVaultItem", primary_key: :membership_hash
+    has_many :item_instances, class_name: "InventoryItemInstance", primary_key: :membership_hash, foreign_key: :membership_hash
     has_many :characters, primary_key: :membership_hash, foreign_key: :membership_hash
 
     def self.unique_keys
       :membership_hash
+    end
+
+    def most_recent_character
+      characters.order(:last_played_at).first
     end
   end
 end
