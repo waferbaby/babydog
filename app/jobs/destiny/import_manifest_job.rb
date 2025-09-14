ad48492eb1bc0c367cd6f2543fddc2bad1426476
Daniel Bogan <d@waferbaby.com>
2025-09-14 05:53:53 +0000

    Moved into the Destiny module

---
+++ app/jobs/destiny/import_manifest_job.rb
@@ -0,0 +1,32 @@
+module Destiny
+  class ImportManifestJob < ApplicationJob
+    queue_as :default
+
+    def perform(*args)
+      sources = Rails.cache.fetch("babydog/manifests/#{Date.today}") do
+        Restiny.download_manifest_json(definitions: [
+          Restiny::ManifestDefinition::STAT,
+          Restiny::ManifestDefinition::DAMAGE_TYPE,
+          Restiny::ManifestDefinition::ENERGY_TYPE,
+          Restiny::ManifestDefinition::TRAIT,
+          Restiny::ManifestDefinition::ICON,
+          Restiny::ManifestDefinition::ITEM_CATEGORY,
+          Restiny::ManifestDefinition::PLUG_SET,
+          Restiny::ManifestDefinition::INVENTORY_BUCKET,
+          Restiny::ManifestDefinition::INVENTORY_ITEM
+        ])
+      end
+
+      sources.each do |manifest, file_path|
+        object_type = "Destiny::#{manifest.gsub(/Destiny|Definition/, '')}".constantize
+
+        begin
+          manifest = JSON.parse(File.read(file_path))
+          object_type.import_collection(manifest.values)
+        rescue JSON::ParserError => e
+          Rails.logger.error("Failed to parse #{file_path}: #{e}")
+        end
+      end
+    end
+  end
+end
