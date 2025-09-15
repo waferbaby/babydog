4b197bc58b51f5c423b0607b5bbbd3d14e08e28e
Daniel Bogan <d@waferbaby.com>
2025-09-15 07:59:43 +0000

    Add the is_named option

--- db/migrate/destiny/migration.rb
+++ db/migrate/destiny/migration.rb
@@ -1,12 +1,16 @@
 module Destiny
   class Migration
-    def self.add_common_fields(table)
+    def self.add_common_fields(table, is_named: true)
       table.bigint :bungie_hash
       table.bigint :index
-      table.string :name
-      table.string :description
       table.boolean :is_redacted, default: false
       table.boolean :is_blacklisted, default: false
+
+      if is_named
+        table.string :name
+        table.string :description
+      end
+
       table.timestamps
 
       table.index :bungie_hash, unique: true
