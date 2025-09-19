3471bb93b9c687a79e4fbac05f189546a2ce6322
Daniel Bogan <d@waferbaby.com>
2025-09-19 07:08:42 +0000

    Pull in profile items too

--- app/controllers/rolls_controller.rb
+++ app/controllers/rolls_controller.rb
@@ -2,16 +2,26 @@ class RollsController < ApplicationController
   before_action :check_authentication
 
   def index
-    @rolls = fetch
+    fetch
+
+    @rolls = current_membership.item_instances
   end
 
   def fetch
     result = profile_for_current_membership([
       Restiny::ComponentType::CHARACTER_INVENTORIES,
+      Restiny::ComponentType::PROFILE_INVENTORIES,
       Restiny::ComponentType::ITEM_STATS
     ]).deep_symbolize_keys
 
-    result.dig(:characterInventories, :data).each do |character_hash, data|
+    items = result.dig(:profileInventory, :data, :items)
+    unless items.blank?
+      Destiny::InventoryItemInstance.import_collection(items, updates: {
+        membership_hash: current_membership.membership_hash
+      })
+    end
+
+    result.dig(:characterInventories, :data)&.each do |character_hash, data|
       data[:items].each do |item|
         Destiny::InventoryItemInstance.import_from_payload(item, updates: {
           membership_hash: current_membership.membership_hash,
