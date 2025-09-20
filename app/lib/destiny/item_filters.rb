5d71e73a855242d8d570779fb01ca22aaedb5139
Daniel Bogan <d@waferbaby.com>
2025-09-20 08:08:06 +0000

    Add the item filters

---
+++ app/lib/destiny/item_filters.rb
@@ -0,0 +1,43 @@
+module Destiny
+  module ItemFilters
+    ALL = {
+      slots: {
+        energy_weapon: "Energy Weapon",
+        kinetic_weapon: "Kinetic Weapon",
+        power_weapon: "Power Weapon"
+      },
+
+      weapons: {
+        weapon: "Weapon",
+        bow: "Bow",
+        fusion_rifle: "Fusion Rifle",
+        glaive: "Glaive",
+        grenade_launcher: "Grenade Launcher",
+        hand_cannon: "Hand Cannon",
+        linear_fusion_rifle: "Linear Fusion Rifle",
+        machine_gun: "Machine Gun",
+        pulse_rifle: "Pulse Rifle",
+        rocket_launcher: "Rocket Launcher",
+        scout_rifle: "Scout Rifle",
+        shotgun: "Shotgun",
+        sidearm: "Sidearm",
+        sniper_rifle: "Sniper Rifle",
+        sword: "Sword",
+        trace_rifle: "Trace Rifle"
+      },
+
+      armours: {
+        armor: "Armor",
+        arms: "Arms",
+        chest: "Chest",
+        legs: "Legs"
+      },
+
+      guardians: {
+        warlock: "Warlock",
+        hunter: "Hunter",
+        titan: "Titan"
+      }
+    }.freeze
+  end
+end
