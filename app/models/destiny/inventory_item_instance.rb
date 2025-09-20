da736bc4410f9165d43b3953f5708b73e45e5e03
Daniel Bogan <d@waferbaby.com>
2025-09-20 22:06:42 +0000

    Add in the scope filters, category associations

--- app/models/destiny/inventory_item_instance.rb
+++ app/models/destiny/inventory_item_instance.rb
@@ -1,13 +1,19 @@
 module Destiny
   class InventoryItemInstance < Base
     belongs_to :membership, foreign_key: :membership_hash, primary_key: :membership_hash
-    has_one :inventory_item, foreign_key: :bungie_hash, primary_key: :inventory_item_hash
+    has_one :inventory_item, foreign_key: :bungie_hash, primary_key: :bungie_hash
+    has_many :inventory_item_categories, foreign_key: :bungie_hash, primary_key: :bungie_hash
+    has_many :categories, class_name: "ItemCategory", through: :inventory_item_categories
 
     delegate :bungie_hash, :name, :description, :icon_url, :has_icon?, to: :inventory_item
 
+    ItemFilters.all.each do |filters|
+      filters.each { |key, name| scope key.to_s.pluralize.to_sym, -> { joins(:categories).merge(ItemCategory.send(key)) } }
+    end
+
     def self.payload_to_attributes(payload)
       {
-        inventory_item_hash: :itemHash,
+        bungie_hash: :itemHash,
         instance_hash: :itemInstanceId,
         inventory_bucket_hash: :bucketHash,
         quantity: :quantity,
@@ -19,7 +25,7 @@ module Destiny
     end
 
     def self.unique_keys
-      [ :membership_hash, :inventory_item_hash ]
+      [ :membership_hash, :bungie_hash ]
     end
   end
 end
