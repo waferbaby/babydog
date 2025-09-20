c0733ae8f88c472397cb98c37f40cf0345a65b94
Daniel Bogan <d@waferbaby.com>
2025-09-20 22:05:35 +0000

    Split out the individual groupings

--- app/lib/destiny/item_filters.rb
+++ app/lib/destiny/item_filters.rb
@@ -1,43 +1,48 @@
 module Destiny
   module ItemFilters
-    ALL = {
-      slots: {
-        energy_weapon: "Energy Weapon",
-        kinetic_weapon: "Kinetic Weapon",
-        power_weapon: "Power Weapon"
-      },
-
-      weapons: {
-        weapon: "Weapon",
-        bow: "Bow",
-        fusion_rifle: "Fusion Rifle",
-        glaive: "Glaive",
-        grenade_launcher: "Grenade Launcher",
-        hand_cannon: "Hand Cannon",
-        linear_fusion_rifle: "Linear Fusion Rifle",
-        machine_gun: "Machine Gun",
-        pulse_rifle: "Pulse Rifle",
-        rocket_launcher: "Rocket Launcher",
-        scout_rifle: "Scout Rifle",
-        shotgun: "Shotgun",
-        sidearm: "Sidearm",
-        sniper_rifle: "Sniper Rifle",
-        sword: "Sword",
-        trace_rifle: "Trace Rifle"
-      },
-
-      armours: {
-        armor: "Armor",
-        arms: "Arms",
-        chest: "Chest",
-        legs: "Legs"
-      },
-
-      guardians: {
-        warlock: "Warlock",
-        hunter: "Hunter",
-        titan: "Titan"
-      }
+    SLOTS = {
+      energy_weapon: "Energy Weapon",
+      kinetic_weapon: "Kinetic Weapon",
+      power_weapon: "Power Weapon"
     }.freeze
+
+    WEAPONS = {
+      weapon: "Weapon",
+      auto_rifle: "Auto Rifle",
+      bow: "Bow",
+      fusion_rifle: "Fusion Rifle",
+      glaive: "Glaive",
+      grenade_launcher: "Grenade Launcher",
+      hand_cannon: "Hand Cannon",
+      linear_fusion_rifle: "Linear Fusion Rifle",
+      machine_gun: "Machine Gun",
+      pulse_rifle: "Pulse Rifle",
+      rocket_launcher: "Rocket Launcher",
+      scout_rifle: "Scout Rifle",
+      shotgun: "Shotgun",
+      sidearm: "Sidearm",
+      sniper_rifle: "Sniper Rifle",
+      sword: "Sword",
+      trace_rifle: "Trace Rifle"
+    }.freeze
+
+    ARMOUR = {
+      armor: "Armor",
+      arms: "Arms",
+      chest: "Chest",
+      legs: "Legs"
+    }
+
+    GUARDIANS = {
+      warlock: "Warlock",
+      hunter: "Hunter",
+      titan: "Titan"
+    }.freeze
+
+    def self.all
+      constants.map do |type|
+        const_get(type)
+      end
+    end
   end
 end
