85f1e5919b3ec8aadaec3b7e72dd967a6700e217
Daniel Bogan <d@waferbaby.com>
2025-09-21 09:15:19 +0000

    Add the icon attributes

--- app/models/destiny/trait.rb
+++ app/models/destiny/trait.rb
@@ -2,5 +2,12 @@ module Destiny
   class Trait < Base
     has_many :inventory_item_traits, foreign_key: :trait_hash, primary_key: :bungie_hash
     has_many :inventory_items, through: :inventory_item_traits
+
+    def self.payload_to_attributes(payload)
+      super(payload).merge({
+        has_icon: [ :displayProperties, :hasIcon ],
+        icon_url: [ :displayProperties, :icon ],
+      })
+    end
   end
 end
