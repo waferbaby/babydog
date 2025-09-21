3696a45e590a1943db84ed10da6a262a56e0c1b8
Daniel Bogan <d@waferbaby.com>
2025-09-21 09:25:23 +0000

    Update the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_20_091815) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_21_090935) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -208,6 +208,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_20_091815) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "has_icon", default: false
+    t.string "icon_url"
     t.index ["bungie_hash"], name: "index_destiny_traits_on_bungie_hash", unique: true
   end
 end
